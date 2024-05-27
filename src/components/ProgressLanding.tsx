d329a3bb08c704383c9329530ae0916a9331ac30
Abhijith Babu <abijithb2002@gmail.com>
2024-05-27 15:41:36 +0530

    change the progress in landing

--- src/components/ProgressLanding.tsx
+++ src/components/ProgressLanding.tsx
@@ -12,7 +12,7 @@ interface CardProps {
 export const Card = ({ children, className }: CardProps) => {
 
     return <motion.section
-        className={`rounded-2xl h-40 flex flex-col min-w-40 max-w-72  items-center justify-center bg-white dark:bg-neutral-950   shadow-[rgba(0,_0,_0,_0.2)_0px_60px_40px_-7px] dark:shadow  overflow-hidden`}
+        className={`rounded-2xl h-40 flex flex-col min-w-40 max-w-72  items-center justify-center   shadow-[rgba(0,_0,_0,_0.2)_0px_60px_40px_-7px] dark:shadow  overflow-hidden`}
         initial={{ opacity: 0, y: -5, }}
         whileInView={{ opacity: 1, y: 0, transition: { duration: 1, delay: 0.1 } }}>
         {children}
@@ -41,20 +41,20 @@ export default function ProgressLanding() {
     return (
         <div className='flex flex-row mt-20 gap-5 w-full justify-between max-w-screen-2xl lg:px-40'>
 
-            <BorderLineCard className='h-40 justify-center items-center overflow-hidden'>
+            <BorderLineCard className='h-40 justify-center items-center overflow-hidden bg-transparent dark:bg-transparent'>
                 <motion.div initial={{ opacity: 0.1 }} whileHover={{ opacity: 0.2 }} className='w-150 h-150 ml-auto' >
-                    <Image src="/gif/blueGradient.gif" alt="Computer man" width={480} height={480} className='w-full h-full blur-sm' />
+                    <Image src="/gif/blueGradient.gif" alt="Computer man" width={480} height={480} className='w-full h-full blur-lg opacity-10' />
                 </motion.div>
                 <div className="flex flex-col lg:w-44 absolute items-center justify-center">
-                    <motion.h1 className=' text-black dark:text-white font-bold text-4xl '>{Universities}</motion.h1>
-                    <p className=' text-black dark:text-gray-300  font-bold'>Universities</p>
+                    <motion.h1 className=' text-black dark:text-white font-bold text-2xl '>{Universities}</motion.h1>
+                    <p className=' text-black dark:text-gray-300 text-lg font-bold'>Universities</p>
                     <p className=' text-black dark:text-gray-400 text-xs text-center'>Lorem Ipsum is a dummy text for country without any sense that is used to fill spaces in graphic</p>
                 </div>
 
             </BorderLineCard>
-            <BorderLineCard className='h-40 justify-center items-center overflow-hidden'>
+            <BorderLineCard className='h-40 justify-center items-center overflow-hidden bg-transparent dark:bg-transparent'>
                 <motion.div initial={{ opacity: 0.1 }} whileHover={{ opacity: 0.2 }} className='w-150 h-150 ml-auto' >
-                    <Image src="/gif/blueGradient.gif" alt="Computer man" width={480} height={480} className='w-full h-full blur-sm' />
+                    <Image src="/gif/blueGradient.gif" alt="Computer man" width={480} height={480} className='w-full h-full blur-lg opacity-10' />
                 </motion.div>
                 <div className="flex flex-col lg:w-44 absolute items-center justify-center">
                     <motion.h1 className=' text-black dark:text-white font-bold text-4xl '>{Students}</motion.h1>
@@ -62,9 +62,9 @@ export default function ProgressLanding() {
                     <p className=' text-black dark:text-gray-400 text-xs text-center'>Lorem Ipsum is a dummy text for country without any sense that is used to fill spaces in graphic</p>
                 </div>
             </BorderLineCard>
-            <BorderLineCard className='h-40 justify-center items-center overflow-hidden'>
+            <BorderLineCard className='h-40 justify-center items-center overflow-hidden bg-transparent dark:bg-transparent'>
                 <motion.div initial={{ opacity: 0.1 }} whileHover={{ opacity: 0.2 }} className='w-150 h-150 ml-auto' >
-                    <Image src="/gif/blueGradient.gif" alt="Computer man" width={480} height={480} className='w-full h-full blur-sm' />
+                    <Image src="/gif/blueGradient.gif" alt="Computer man" width={480} height={480} className='w-full h-full blur-lg opacity-10' />
                 </motion.div>
                 <div className="flex flex-col lg:w-44 absolute items-center justify-center">
                     <motion.h1 className=' text-black dark:text-white font-bold text-4xl '>{Countries}</motion.h1>
@@ -72,14 +72,14 @@ export default function ProgressLanding() {
                     <p className=' text-black dark:text-gray-400 text-xs text-center'>Lorem Ipsum is a dummy text for country without any sense that is used to fill spaces in graphic</p>
                 </div>
             </BorderLineCard>
-            <BorderLineCard className='h-40 justify-center items-center overflow-hidden'>
+            <BorderLineCard className='h-40 justify-center items-center overflow-hidden bg-transparent dark:bg-transparent'>
                 <motion.div className="flex flex-col lg:w-44 absolute items-center justify-center " initial={{ scale: 1 }} whileHover={{ scale: 1.5 }} >
                     <motion.h1 initial={{ scale: 1 }} whileHover={{ scale: 1.5 }} className=' text-black dark:text-white font-bold text-4xl '>{Years}</motion.h1>
                     <p className=' text-black dark:text-gray-300  font-bold'>Years of Experience </p>
                     <p className=' text-black dark:text-gray-400 text-xs text-center'>Lorem Ipsum is a dummy text for country without any sense that is used to fill spaces in graphic</p>
                 </motion.div>
-                <motion.div initial={{ opacity: 0.1 }} whileHover={{ opacity: 0.2 }} className='w-150 h-150 ml-auto' >
-                    <Image src="/gif/blueGradient.gif" alt="Computer man" width={480} height={480} className='w-full h-full blur-sm' />
+                <motion.div initial={{ opacity: 0.01 }} whileHover={{ opacity: 0.02 }} className='w-150 h-150 ml-auto' >
+                    <Image src="/gif/blueGradient.gif" alt="Computer man" width={480} height={480} className='w-full h-full blur-lg ' />
                 </motion.div>
             </BorderLineCard>
 
